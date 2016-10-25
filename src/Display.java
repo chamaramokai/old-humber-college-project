3c7f4c1a460422a7d251fd05d6d0eb2770cca91c
ani1797 <starfire1797@gmail.com>
2016-10-25 11:07:44 -0400

    Added index html(incomplete)

---
+++ src/Display.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/**
+ * This Class generates 
+ * @author Anirudh
+ *
+ */
+public class Display {
+	public enum Type
+	{
+		SUCCESS,
+		INFO,
+		ERROR,
+		WARNING
+	}
+	
+	public Display(Type type)
+	{
+		switch(type)
+		{
+		case ERROR:
+			break;
+		case INFO:
+			break;
+		case SUCCESS:
+			break;
+		case WARNING:
+			break;
+		default:
+			break;
+			
+		}
+	}
+	
+}
