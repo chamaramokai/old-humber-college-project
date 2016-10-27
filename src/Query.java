53611b92fa4cd3e2e09e569841374be621016494
ani1797 <starfire1797@gmail.com>
2016-10-26 20:05:16 -0400

    Database and login working! Error messages also functioning
    I have added the mysql connector and got the database methods and tested
    them for functionality. Error messages are also working on the home
    page. Error are given out in a simple strings. If any questions, let me
    know.

--- src/Query.java
+++ src/Query.java
@@ -1,4 +1,18 @@
 
 public enum Query {
-	
+	AUTHENTICATE("SELECT * FROM USER WHERE USERNAME='%s' AND PASSWORD='%s'"),
+	SELECT_ALL_FROM("SELECT * FROM %s WHERE USERNAME='%s'");
+	;
+
+	  private final String command;
+
+	  private Query(String command) {
+	    this.command = command;
+	  }
+
+	  @Override
+	  public String toString() {
+	     return command;
+	  }
+	  
 }
